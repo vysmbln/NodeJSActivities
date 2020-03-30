d81245973a2913c11956a8b682edcbd24c93b30c
vysmbln <vysmbln@users.noreply.github.com>
2020-03-30 14:39:15 -0400

    first commit

---
+++ watchit/index.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+const debounce = require('lodash.debounce');
+const chokidar = require('chokidar');
+const program = require('caporal');
+
+program
+.version('0.0.1')
+.argument('[filename', 'Name of a file to execute')
+.action(args => {
+    console.log(args);
+});
+
+program.parse(process.argv);
+
+
+
+
+
+const start = debounce(() =>{
+    console.log('starting users program');
+}, 100);
+
+chokidar.watch
+    ('.')
+    .on('add', start)
+    .on('change', () => console.log('FILE CHANGED'))
+    .on('unlink', () => console.log('FILE UNLINKED'));
+
