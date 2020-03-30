3ad7331bb6541e8bbcddf7d85c910e1fbc087da6
vysmbln <vysmbln@users.noreply.github.com>
2020-03-30 16:14:07 -0400

    spawn

--- watchit/index.js
+++ watchit/index.js
@@ -3,27 +3,42 @@
 const debounce = require('lodash.debounce');
 const chokidar = require('chokidar');
 const program = require('caporal');
-
+const fs = require('fs');
+const { spawn } = require('child_process')
+const chalk = require('chalk')
 program
 .version('0.0.1')
 .argument('[filename', 'Name of a file to execute')
-.action(args => {
-    console.log(args);
+.action(async ({ filename }) => {
+    const name = filename || 'index.js';
+
+    try {
+        await fs.promises.access(name);
+    } catch(err) {
+        throw new Error(`Could not find the file ${name}` )
+    }
+
+    let proc;
+    
+    const start = debounce(() => {
+        if(proc) {
+            proc.kill();
+        }
+        console.log(chalk.blue('>>>>>Starting Process ...'));
+        proc = spawn('node', [name], { stdio: 'inherit' });
+    }, 100);
+
+    chokidar
+        .watch('.')
+        .on('add', start)
+        .on('change', start)
+        .on('unlink',start);
+
+
 });
 
 program.parse(process.argv);
 
 
 
-
-
-const start = debounce(() =>{
-    console.log('starting users program');
-}, 100);
-
-chokidar.watch
-    ('.')
-    .on('add', start)
-    .on('change', () => console.log('FILE CHANGED'))
-    .on('unlink', () => console.log('FILE UNLINKED'));
 
